671e92d8ee11bac565cca11950a3539c8943ecff
unknown <afsanchezsa@unal.edu.co>
2019-09-28 17:52:05 -0500

    starting the server in index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+
+
+//settings
+app.set('port',process.env.PORT||3000); //set the number port for listen request
+//Middleware
+app.use(express.json());//let us access to the info sended by json format
+
+///Routes
+
+app.listen(app.get('port'),()=>{
+    console.log('server on port',app.get('port'));
+});
+
+    
