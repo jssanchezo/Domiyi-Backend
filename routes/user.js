33272084bcf7d0d4302ec61656ff6c5c74cd7c6c
unknown <afsanchezsa@unal.edu.co>
2019-09-28 22:05:11 -0500

    fixing the route for users

--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,5 @@ const express=require('express');
 const router=express.Router();
 
 const UserController= require('../controllers/user.js');
-router.get('/',UserController.getAll);
+router.get('/users',UserController.getAll);
 module.exports=router;
