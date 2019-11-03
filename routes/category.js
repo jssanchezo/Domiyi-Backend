4639101e1974e16736c5296a49929d5d37bd2649
unknown <afsanchezsa@unal.edu.co>
2019-11-03 18:55:53 -0500

    implemented route, repository and controller from product category

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const ProtectRoute=require('../authentication/protectRouteMiddleware');
 //we match all routes with the functions wich it executes
 
-const ProductStatusController= require('../controllers/productsStatus.js');
-router.get('/productsStatus' ,ProductStatusController.getAll);
+const CategoryController= require('../controllers/category.js');
 
+router.get('/categories',CategoryController.getAll);
 module.exports= router;
