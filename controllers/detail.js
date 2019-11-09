a36ef682d3dec5265254424ecebb571bf2fe2d84
unknown <afsanchezsa@unal.edu.co>
2019-11-09 16:17:39 -0500

    getByIdOrder detail repository

--- controllers/detail.js
+++ controllers/detail.js
@@ -24,7 +24,7 @@ const DetailController = {
        if(details==null){
            res.status(400).send("esta vacio");
        }else{
-                   
+              
         res.status(200).json(details);
        }
        
