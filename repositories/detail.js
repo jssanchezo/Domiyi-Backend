0d54ce8fbc3e8ec2efff91094ea7ad92bbb9c2db
sv1024 <vejars1024@gmail.com>
2019-11-09 20:30:45 -0500

    Detail register working

--- repositories/detail.js
+++ repositories/detail.js
@@ -1,58 +1,56 @@
-const connection=require('../Database-Utilities/Connection.js');
-const ProductTable=require('../Database-Utilities/Products.js');
-const Detail=require('../models/Detail');
-const Sequelize=require('sequelize');
-const sequelize=require('../Database-Utilities/SequelizeConnection');
+const connection = require('../Database-Utilities/Connection.js');
+const ProductTable = require('../Database-Utilities/Products.js');
+const Detail = require('../models/Detail');
+const Sequelize = require('sequelize');
+const sequelize = require('../Database-Utilities/SequelizeConnection');
 
-const DetailRepository={
-    
+const DetailRepository = {
+
     //
     //
     async SelectIdsProductOfferByIdOrder(idOrder) {
-      try{
-        var idsProductOffer=await Detail.findAll({
-         attributes:['idProductOffer'],
-          where:{
-            idOrder:idOrder
-          }
-        });
-        return idsProductOffer;
-      }catch(e){
-          return null;
-      }
-        },
-
-
-
-    async register(req,res){
-      try{
-        detail=await Detail.create({
-            //id:req.body.id,//si no se agrega se ejecuta el autoincrement en la bd
-            idOrder:req.body.idOrder,
-            idProductOffer:req.body.idProductOffer,
-            quantity:req.body.quantity,
-            observation:req.body.observation,
-            unitprice:req.body.unitprice            
-         
-        });
-        res.status(201).json(detail);
-      }catch(e){
-          res.status(400).send("se produjo un error");
-          console.log(e);
-      }
+        try {
+            var idsProductOffer = await Detail.findAll({
+                attributes: ['idProductOffer'],
+                where: {
+                    idOrder: idOrder
+                }
+            });
+            return idsProductOffer;
+        } catch (e) {
+            return null;
+        }
     },
-  async  SelectByOrderId(idOrder){
-    try{
-      const details= await sequelize.query("SELECT * FROM productAndDetail where idOrder="+idOrder,{ type: Sequelize.QueryTypes.SELECT})
-      
-      return details;
-    }catch(e){
-      
-return null;
-    }
-      
-    
-      
+
+
+    async register(req, res) {
+        try {
+            detail = await Detail.create({
+                //id:req.body.id,//si no se agrega se ejecuta el autoincrement en la bd
+                idOrder: req.body.idOrder,
+                idProductOffer: req.body.idProductOffer,
+                quantity: req.body.quantity,
+                observation: req.body.observation,
+                unitPrice: req.body.unitPrice
+
+            });
+            res.status(201).json(detail);
+        } catch (e) {
+            res.status(400).send("se produjo un error");
+            console.log(e);
+        }
+    },
+    async SelectByOrderId(idOrder) {
+        try {
+            const details = await sequelize.query("SELECT * FROM productAndDetail where idOrder=" + idOrder, {type: Sequelize.QueryTypes.SELECT})
+
+            return details;
+        } catch (e) {
+
+            return null;
+        }
+
+
     }
 }
-module.exports=DetailRepository;
+module.exports = DetailRepository;
