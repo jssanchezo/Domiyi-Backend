543d542acbf97668cbb4844598aec7891b4a55ed
Edinson Vega <evegab@unal.edu.co>
2019-11-17 02:55:29 -0500

    pruebas unitarias en username y phone

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express=require('express');
+const express = require('express');
 //para tests
 //var request =require('supertest');
 //
-const PromoFactory=require('./POJO/PromoFactory');
-const app=express();
-const users_routes=require('./routes/user.js');
+const PromoFactory = require('./POJO/PromoFactory');
+const app = express();
+const users_routes = require('./routes/user.js');
 
 //we call the routes for the frontend
-const company_routes=require('./routes/company.js');
-const productsStatus_routes=require('./routes/productsStatus.js');
-const companiesStatus_routes=require('./routes/companiesStatus.js');
-const order_routes=require('./routes/order')
-const category_routes=require('./routes/category');
-const product_routes=require('./routes/product.js');
-const authentication_route=require('./authentication/authenticationRoute.js');
-const detail_routes=require('./routes/detail');
-const productOffer_routes=require('./routes/ProductOffer');
-const cors=require('cors');//solve problems with cors on requests
+const company_routes = require('./routes/company.js');
+const productsStatus_routes = require('./routes/productsStatus.js');
+const companiesStatus_routes = require('./routes/companiesStatus.js');
+const order_routes = require('./routes/order')
+const category_routes = require('./routes/category');
+const product_routes = require('./routes/product.js');
+const authentication_route = require('./authentication/authenticationRoute.js');
+const detail_routes = require('./routes/detail');
+const productOffer_routes = require('./routes/ProductOffer');
+const cors = require('cors'); //solve problems with cors on requests
 const UserController = require('./controllers/user.js');
 const CompanyController = require('./controllers/company.js');
-const ProductController=require('./controllers/product');
+const ProductController = require('./controllers/product');
 
 //settings
-app.set('port',process.env.PORT||3000); //set the number port for listen request
+app.set('port', process.env.PORT || 3000); //set the number port for listen request
 //Middleware
 app.use(cors());
-app.use(express.json());//let us access to the info sended by json format
+app.use(express.json()); //let us access to the info sended by json format
 
 ///Routes: use the routes
 app.use(users_routes);
@@ -40,18 +40,18 @@ app.use(order_routes);
 app.use(detail_routes);
 app.use(productOffer_routes);
 //starting the server
-promofactory=new PromoFactory();
-var discount=promofactory.getPromo("discount");
-var product={price:3000};
-product=discount.applyPromo(product,0.1);
-console.log("price with 10 % off is "+product.price);
-var minuspricepromo=promofactory.getPromo("minusprice");
-product=minuspricepromo.applyPromo(product,1000);
-console.log("price with 10 % off is and minus 1000 is "+product.price);
+promofactory = new PromoFactory();
+var discount = promofactory.getPromo("discount");
+var product = { price: 3000 };
+product = discount.applyPromo(product, 0.1);
+console.log("price with 10 % off is " + product.price);
+var minuspricepromo = promofactory.getPromo("minusprice");
+product = minuspricepromo.applyPromo(product, 1000);
+console.log("price with 10 % off is and minus 1000 is " + product.price);
 
-  //  prom.applyPromo();
-app.listen(app.get('port'),()=>{
-    console.log('server on port',app.get('port'));
+//  prom.applyPromo();
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
 });
 /*
 function createApp() {
@@ -65,23 +65,21 @@ function createApp() {
 }
  */
 
-/*
+/* 
 
 describe('Is email correct', function() {
 
     // This is the name of the test
     it('for test@gmail.com is validEmail must return true ', function(done) {
         var correctEmail = UserController.validateEmail("test@gmail.com")
-            if (correctEmail) {
-                // If the behavior is as expected, call done with no argument.
-                done();
-            }
-
-            else {
-                // Otherwise, call done with an error.
-                done(new Error("Not sure what's happened."));
-            }
-            // We want this test to pass.
+        if (correctEmail) {
+            // If the behavior is as expected, call done with no argument.
+            done();
+        } else {
+            // Otherwise, call done with an error.
+            done(new Error("Not sure what's happened."));
+        }
+        // We want this test to pass.
     });
 
 });
@@ -93,9 +91,7 @@ describe('Its not a valid password', function() {
         if (!correctPassword) {
             // If the behavior is as expected, call done with no argument.
             done();
-        }
-
-        else {
+        } else {
             // Otherwise, call done with an error.
             done(new Error("Not sure what's happened."));
         }
@@ -113,9 +109,7 @@ describe('Is IdAdmin number correct', function() {
         if (correctIdAdmin) {
             // If the behavior is as expected, call done with no argument.
             done();
-        }
-
-        else {
+        } else {
             // Otherwise, call done with an error.
             done(new Error("Not sure what's happened."));
         }
@@ -134,9 +128,7 @@ describe('Is IdAdmin type correct', function() {
         if (correctTypeIdAdmin) {
             // If the behavior is as expected, call done with no argument.
             done();
-        }
-
-        else {
+        } else {
             // Otherwise, call done with an error.
             done(new Error("Not sure what's happened."));
         }
@@ -158,13 +150,11 @@ describe('The price  is  valid', function() {
     // This is the name of the test
     it('for price -4567 Should return  false', function(done) {
         var correctPrice = ProductController.isValidPrice(-4567);
-        
+
         if (!correctPrice) {
             // If the behavior is as expected, call done with no argument.
             done();
-        }
-
-        else {
+        } else {
             // Otherwise, call done with an error.
             done(new Error("Not sure what's happened."));
         }
@@ -178,13 +168,11 @@ describe('The description is not empty', function() {
     // This is the name of the test
     it('for "" Should return  false', function(done) {
         var correctDescription = ProductController.isValidDescription("");
-        
+
         if (!correctDescription) {
             // If the behavior is as expected, call done with no argument.
             done();
-        }
-
-        else {
+        } else {
             // Otherwise, call done with an error.
             done(new Error("Not sure what's happened."));
         }
@@ -194,8 +182,44 @@ describe('The description is not empty', function() {
 
 });
 
-*/
+describe('Comprobar campos de inicio de sesion no esten vacios', function() {
 
+    // This is the name of the test
+    it('for "" Should return  false', function(done) {
+        var correctUserName = UserController.isValidUserName("");
+
+        if (correctUserName) {
+            // If the behavior is as expected, call done with no argument.
+            done();
+        } else {
+            // Otherwise, call done with an error.
+            done(new Error("Username no es valido"));
+        }
+        // We want this test to pass.
+    });
+
+});
+
+describe('The phone  is non negative', function() {
+
+    // This is the name of the test
+    it('for phone -2076856 Should return  false', function(done) {
+        var correctPhone = UserController.isValidPhone(-2076856);
+
+        if (!correctPhone) {
+            // If the behavior is as expected, call done with no argument.
+            done();
+        } else {
+            // Otherwise, call done with an error.
+            done(new Error("Not sure what's happened."));
+        }
+        // We want this test to pass.
+    });
+
+
+});
+
+ */
 
 
 
@@ -216,8 +240,3 @@ const User=Cat.User;
 const Prueba=User.findAll({
 attributes:['id']
 });*/
-
-
-
-
-    
