986ed5ac2f241c72c75f6cb7a4b1feb81f9ff4d9
unknown <afsanchezsa@unal.edu.co>
2019-11-17 16:53:32 -0500

    back to local

--- Database-Utilities/DatabaseDefinition.js
+++ Database-Utilities/DatabaseDefinition.js
@@ -1,8 +1,8 @@
 const Constants = {
-    host: 'bqgr2cirsykagvh6xt6c-mysql.services.clever-cloud.com',
-    user: 'ulbibdyyhb9hs6ek',
-    password: 'bhKPJzV8AvuovJI3OW4y',
-    database: 'bqgr2cirsykagvh6xt6c'//fix the main parameters for the connection
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'domiyi_dbp'//fix the main parameters for the connection
 
 }
 module.exports = Constants;//export the constants
